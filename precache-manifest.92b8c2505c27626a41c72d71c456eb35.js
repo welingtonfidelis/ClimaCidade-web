aafa8a5b5d8d65a1f1259abed1cf56c0f012c030
Welington Fidelis <welingtonfidelis@gmail.com>
2020-06-24 11:04:06 -0300

    Updates

---
+++ precache-manifest.92b8c2505c27626a41c72d71c456eb35.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1de133f74b6ca1f31e80fbb9321ad2a4",
+    "url": "/ClimaCidade-web/index.html"
+  },
+  {
+    "revision": "f690b0af74ebb94603ba",
+    "url": "/ClimaCidade-web/static/css/main.42123765.chunk.css"
+  },
+  {
+    "revision": "7d20a0b24663622f9611",
+    "url": "/ClimaCidade-web/static/js/2.f5caff56.chunk.js"
+  },
+  {
+    "revision": "3be25bf51018a491ef7f73dfd2dd74c4",
+    "url": "/ClimaCidade-web/static/js/2.f5caff56.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f690b0af74ebb94603ba",
+    "url": "/ClimaCidade-web/static/js/main.58e76b22.chunk.js"
+  },
+  {
+    "revision": "0b2b81e7d430732bb86d",
+    "url": "/ClimaCidade-web/static/js/runtime-main.f1dec2ff.js"
+  },
+  {
+    "revision": "deef9858f15314ee0dd20bb9bd67eeec",
+    "url": "/ClimaCidade-web/static/media/bgClounds.deef9858.svg"
+  }
+]);
